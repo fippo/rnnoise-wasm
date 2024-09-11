cb1fb6c451de52ecb48462cb772d5b329ad6fed3
Philipp Hancke <philipp.hancke@googlemail.com>
2024-09-11 07:08:13 -0700

    borrow more jitsi code

--- example/worker.js
+++ example/worker.js
@@ -1,18 +1,26 @@
 import RNNoise from './rnnoise.js';
+
 let wasmInterface;
 let context;
+let wasmPcmInput;
+let wasmPcmInputF32Index;
 async function transform(encodedFrame, controller) {
     const buffer = new Float32Array(encodedFrame.numberOfFrames * encodedFrame.numberOfChannels);
     for (let channel = 0; channel < encodedFrame.numberOfChannels; channel++) {
       const offset = encodedFrame.numberOfFrames * channel;
       const samples = buffer.subarray(offset, offset + encodedFrame.numberOfFrames);
       encodedFrame.copyTo(samples, {planeIndex: channel, format: 'f32-planar'});
-      console.log(samples);
-      wasmInterface._rnnoise_process_frame(
+      for (let i = 0; i < encodedFrame.numberOfFrames; i++) {
+        wasmInterface.HEAPF32[wasmPcmInputF32Index + i] = samples[i] * 32768;
+      }
+      const res = wasmInterface._rnnoise_process_frame(
         context,
-        samples,
-        samples,
+        wasmPcmInput,
+        wasmPcmInput
       );
+      for (let i = 0; i < encodedFrame.numberOfFrames; i++) {
+        samples[i] = wasmInterface.HEAPF32[wasmPcmInputF32Index + i] / 32768;
+      }
     }
     encodedFrame.data = buffer;
     controller.enqueue(encodedFrame);
@@ -21,6 +29,8 @@ async function transform(encodedFrame, controller) {
 onmessage = async (event) => {
     wasmInterface = await RNNoise();
     context = wasmInterface._rnnoise_create();
+    wasmPcmInput = wasmInterface._malloc(480 * 4);
+    wasmPcmInputF32Index = wasmPcmInput >> 2;
     const source = event.data.source;
     const sink = event.data.sink;
     const transformer = new TransformStream({transform});
